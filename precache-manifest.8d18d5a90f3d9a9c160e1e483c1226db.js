2b73a3f17c2f28544d60c9069cb1dfac54160a6e
fahning <smile@TI-06.smilesaude.local>
2020-08-05 15:07:36 -0300

    First Commit2

---
+++ precache-manifest.8d18d5a90f3d9a9c160e1e483c1226db.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445f05d60feb27607638",
+    "url": "/css/app.19ee7527.css"
+  },
+  {
+    "revision": "fbf926bd80d090a3402a",
+    "url": "/css/chunk-vendors.20721517.css"
+  },
+  {
+    "revision": "6e4493fd20c56b1143476d54c98990f3",
+    "url": "/favicon-xyz.png"
+  },
+  {
+    "revision": "0b07f5a484a9fb24025083dda023c7e8",
+    "url": "/img/PHPMVC.0b07f5a4.png"
+  },
+  {
+    "revision": "6d94b0e7d52f7e832f2bfc708129acb1",
+    "url": "/img/POOPHP.6d94b0e7.png"
+  },
+  {
+    "revision": "e2fdd2db1faa0b47b65739cd75714b3c",
+    "url": "/img/cursoLaravel.e2fdd2db.png"
+  },
+  {
+    "revision": "b00bf2c6764cfc697f08197f3c9e63d3",
+    "url": "/img/foto1.b00bf2c6.png"
+  },
+  {
+    "revision": "e761278b31a6d01153cf51f49e5dd241",
+    "url": "/img/foto2.e761278b.png"
+  },
+  {
+    "revision": "89f1cb1a7487ff4143d3acee38d47dc6",
+    "url": "/img/foto3.89f1cb1a.png"
+  },
+  {
+    "revision": "e7b90f432acda5e9326b5f77469ca450",
+    "url": "/img/fotoPerfil.e7b90f43.jpg"
+  },
+  {
+    "revision": "7ce11b1bbc4d9a9af7c1b0a991bb17af",
+    "url": "/img/laravelApiRest.7ce11b1b.png"
+  },
+  {
+    "revision": "1e170f93dda5d21cba87ef7990a13842",
+    "url": "/img/logo.1e170f93.png"
+  },
+  {
+    "revision": "2becf5824f80e72e54aa2c169a107843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "445f05d60feb27607638",
+    "url": "/js/app.31cd5b4a.js"
+  },
+  {
+    "revision": "fbf926bd80d090a3402a",
+    "url": "/js/chunk-vendors.2ef1126d.js"
+  },
+  {
+    "revision": "7e411c584383b91019e08a944f72e40d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
